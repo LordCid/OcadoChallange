528e9eda29455d0663632dc32e0a42e8ea0b8bad
albert.cid.bordas <albert.cid.bordas@accenture.com>
2020-04-08 20:30:37 +0200

    Constants fix

--- app/src/main/java/com/example/ocadochallenge/Constants.kt
+++ app/src/main/java/com/example/ocadochallenge/Constants.kt
@@ -2,5 +2,5 @@ package com.example.brewdogbeers
 
 object Constants {
     const val PREF_FILE_NAME = "Preferences"
-    const val BASE_URL = "https://my-json-server.typicode.com/ocadotechnology/mobile-challenge"
+    const val BASE_URL = "https://my-json-server.typicode.com/ocadotechnology/mobile-challenge/"
 }
