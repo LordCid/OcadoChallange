c8a98d2526e7f42b74c4f7256b6011f6ce0f0433
albert.cid.bordas <albert.cid.bordas@accenture.com>
2020-04-09 14:52:57 +0200

    Product mapper implementation

--- app/src/main/java/com/example/ocadochallenge/domain/ProductMapper.kt
+++ app/src/main/java/com/example/ocadochallenge/domain/ProductMapper.kt
@@ -1,4 +1,18 @@
 package com.example.ocadochallenge.domain
 
-class ProductMapper {
+import com.example.ocadochallenge.domain.model.Product
+import com.example.ocadochallenge.repository.rest.model.ProductNetworkModel
+
+class ProductMapper: Mapper<ProductNetworkModel, Product> {
+    override fun map(model: ProductNetworkModel): Product {
+        return Product(
+            id = model.id ?:0,
+            price = model.price ?:"",
+            title = model.title ?:"",
+            size = model.size ?:"",
+            imageUrl = model.imageUrl ?:"",
+            description = model.description ?:"",
+            allergyInformation = model.allergyInformation ?:""
+        )
+    }
 }
--- app/src/test/java/com/example/ocadochallenge/domain/ProductMapperTest.kt
+++ app/src/test/java/com/example/ocadochallenge/domain/ProductMapperTest.kt
@@ -1,12 +1,75 @@
 package com.example.ocadochallenge.domain
 
+import com.example.ocadochallenge.domain.model.Product
+import com.example.ocadochallenge.getNetworkProductById
+import com.example.ocadochallenge.getProductById
+import com.example.ocadochallenge.repository.rest.model.ProductNetworkModel
 import org.junit.Before
 
 import org.junit.Assert.*
+import org.junit.Test
 
 class ProductMapperTest {
 
+    private lateinit var sut: Mapper<ProductNetworkModel, Product>
+
     @Before
     fun setUp() {
+        sut = ProductMapper()
     }
+
+    @Test
+    fun `Should map network model to domain model`() {
+        val someId = 12345
+        val networkModel = getNetworkProductById(someId)
+        val expected = getProductById(someId)
+
+        val actual = sut.map(networkModel)
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `Should map OTHER network model to domain model`() {
+        val someId = 6789
+        val networkModel = getNetworkProductById(someId)
+        val expected = getProductById(someId)
+
+        val actual = sut.map(networkModel)
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `Should map network null values to empty string or 0 values`() {
+        val networkModel = getNullValuesNetworkModel()
+        val expected = getEmptyProductModel()
+
+        val actual = sut.map(networkModel)
+
+        assertEquals(expected, actual)
+    }
+
+    private fun getNullValuesNetworkModel() = ProductNetworkModel(
+        id = null,
+        price = null,
+        title = null,
+        size = null,
+        imageUrl = null,
+        description = null,
+        allergyInformation = null
+    )
+
+
+    private fun getEmptyProductModel() = Product(
+        id = 0,
+        price = "",
+        title = "",
+        size = "",
+        imageUrl = "",
+        description = "",
+        allergyInformation = ""
+    )
+
 }
+
