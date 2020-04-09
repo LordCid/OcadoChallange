3f0bcae8a6a32f842fb509b46bef82c68b43bd6e
albert.cid.bordas <albert.cid.bordas@accenture.com>
2020-04-09 14:55:21 +0200

    Added new method in Api service for getting products by id

--- app/src/main/java/com/example/ocadochallenge/repository/rest/ApiService.kt
+++ app/src/main/java/com/example/ocadochallenge/repository/rest/ApiService.kt
@@ -2,18 +2,22 @@ package com.example.ocadochallenge.repository.rest
 
 import com.example.brewdogbeers.Constants.BASE_URL
 import com.example.ocadochallenge.repository.rest.model.ProductClusterListNetworkModel
+import com.example.ocadochallenge.repository.rest.model.ProductNetworkModel
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
-import retrofit2.http.Query
+import retrofit2.http.Path
 
 interface ApiService {
 
     @GET("products")
-    fun getProducts(): Call<ProductClusterListNetworkModel>
+    fun getProductList(): Call<ProductClusterListNetworkModel>
+
+    @GET("product/{id}")
+    fun getProduct(@Path("id") id: Int): Call<ProductNetworkModel>
 
     companion object {
         fun create(): ApiService {
